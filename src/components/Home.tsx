908d1ba71fbad7cb9b50825ab84550c93ae8a27c
Lucas Herculano Rodrigues <48929939+Lucaswhy@users.noreply.github.com>
2021-11-26 09:01:48 -0300

    starting home layout

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,56 +1,11 @@
 import React, { ReactElement } from "react";
 import axios from 'axios';
-
-interface IUser {
-    Id: Number,
-    Name: String,
-    Email: String,
-    Avatar?: String,
-    Music?: any,
-    Playlist?: any,
-    Album?: any
-}
-
-interface IState {
-    error: Boolean,
-    isLoaded: Boolean,
-    items: Array<IUser>; 
-}
-
-class Home extends React.Component<{}, IState> {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-          error: false,
-          isLoaded: false,
-          items: []
-        };
-    }
-
-    componentDidMount() {
-        axios.post(`http://localhost:8080/api/login/`)
-          .then(res =>{
-            console.log(res.data);
-              this.setState({
-                isLoaded: true,
-                error: res.data.error,
-                items: res.data.value
-              })
-          },(error: String) => {
-            this.setState({
-                isLoaded: true,
-            });
-          });
-    }
+import IUser from './template'
     
-    
-    render(){
-        const { error, isLoaded, items } = this.state;
-          let divArtist : any;
-        if (error === true) {
-            return <div>Error: Ocorreu um erro, tente novamente mais tarde! :( </div>
-          } else if (!isLoaded) {
+ function Home (items: any,isLoaded: Boolean){
+   console.log(items + " " + isLoaded)
+        let divArtist : any;
+        if (!isLoaded) {
             return <h2>Carregando seus artistas! =)</h2>;
           } else {
             if(items === null || items === undefined) divArtist = <h2>Você não está logado!</h2>
@@ -64,7 +19,6 @@ class Home extends React.Component<{}, IState> {
               </div>
             )
           }
-        }
-    }
+}
 
 export default Home;
--- src/components/template.tsx
+++ src/components/template.tsx
@@ -2,10 +2,55 @@ import React from "react";
 import LeftMenu from './LeftMenu';
 import Header from './Header';
 import Home from './Home';
+import axios from 'axios';
 
-class Template extends React.Component<{}> {
+interface IUser {
+    Id: Number,
+    Name: String,
+    Email: String,
+    Avatar?: String,
+    Music?: any,
+    Playlist?: any,
+    Album?: any
+}
+interface IState {
+    error: Boolean,
+    isLoaded: Boolean,
+    items: Array<IUser>; 
+}
+class Template extends React.Component<{}, IState> {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          error: false,
+          isLoaded: false,
+          items: []
+        };
+    }
+
+    componentDidMount() {
+        axios.post(`http://localhost:8080/api/login/`)
+          .then(res =>{
+            console.log(res.data);
+              this.setState({
+                isLoaded: true,
+                error: res.data.error,
+                items: res.data.value
+              })
+          },(error: String) => {
+            this.setState({
+                isLoaded: true,
+            });
+          });
+    }
     
     render(){
+        const { error, isLoaded, items } = this.state;
+        if (error === true) {
+            return <div>Error: Ocorreu um erro, tente novamente mais tarde! :( </div>
+        }
+        else{
             return (
                 <div className="template">
                     <div className="template__menu">
@@ -16,11 +61,12 @@ class Template extends React.Component<{}> {
                             <Header/>
                         </div>
                         <div className="container">
-                            <Home/>
+                            <Home items={items} isLoaded={isLoaded}/>
                         </div>
                     </div>
                 </div>
             );
+        }
         }
     }
 
